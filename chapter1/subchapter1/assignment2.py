70cefff49d2c26c53e42ff64b9aab5bb34455ef3
Martin Lidén <martin.liden@live.se>
2022-01-26 01:01:16 +0100

    Adding assignment 2 and blank .py file for assignment 3.

---
+++ chapter1/subchapter1/assignment2.py
@@ -0,0 +1,11 @@
+a = 65
+b = 7
+
+print("a)")
+print(f'The number {a} is {hex(a)} in hex.\nThe number {b} is {hex(b)} in hex.\n\n')
+
+print("b)")
+print(f'The number {a} divided by {b} is {a/b:.3f}.\n\n')
+
+print("c)\n")
+print(f'The reminder after dividing {a} with {b} is {a%b}.\n\n')
