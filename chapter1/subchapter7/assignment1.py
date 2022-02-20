23b49129ac8f6d3ab8454f4457ba4d16f6b45514
Martin Lidén <martin.liden@live.se>
2022-02-20 19:33:09 +0100

    Finalization of ch1sub7 assignment1. hardcoded input as a list. Isnt taken from terminal.

--- chapter1/subchapter7/assignment1.py
+++ chapter1/subchapter7/assignment1.py
@@ -1,20 +1,34 @@
+def generateEmptyMatrix(A):
+    output = []
+    for i in range(len(A)):
+        output.append([])
+        for j in range(len(A[1])):
+            output[i].append([None])
+
+    return output
+
 def addMatrices(A, B):
-    output = [[],[],[]]
+    output = generateEmptyMatrix(A)
     for i in range(len(A)):
         for j in range(len(A[1])):
-            output[i].append(A[i][j]+B[i][j])
+            output[i][j] = A[i][j] + B[i][j]
+    return output
 
 def substractMatrices(A, B):
-    output = [[],[],[]]
+    output = generateEmptyMatrix(A)
     for i in range(len(A)):
         for j in range(len(A[1])):
-            output[i].append(A[i][j]-B[i][j])
+            output[i][j] = A[i][j] - B[i][j]
+    return output
 
 #mat1 = list(input('Input the first matrix: '))
 #mat2 = list(input('Input the second matrix: '))
-mat1 = list('[[1,1],[1,1]]')
-mat2 = list('[[1,1],[2,3]]')
-# Need to convert this list och chars to something of use!
+#mat1 = list('[[1,1],[1,1]]')
+#mat2 = list('[[1,1],[2,3]]')
+# Need to convert this list of chars to something of use!
+
+mat1 = [[1,1],[1,1]]
+mat2 = [[1,1],[2,3]]
 
 if len(mat1) == len(mat2) and len(mat1[1]) == len(mat2[1]):
     cond = int(input('For addition type 1, all others are substraction.: '))
