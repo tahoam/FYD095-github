0c781666405c166fc66554934a9fbb2bc9d64d87
Martin Lidén <martin.liden@live.se>
2022-03-06 16:47:49 +0100

    Added ch3sub4 assignment 2. Use SciPy and calculate an integral with the trapezoid method.

---
+++ chapter3/subchapter4/assignment2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy.integrate as intg
+import math
+
+def integrate_with_trapeziod(func, boundaries, n_dx):
+    a,b = boundaries
+    x_val = np. linspace(a,b, n_dx - 1)
+    y_val = []
+
+    for x in x_val:
+        y_val.append(func(x))
+
+    integral = intg.trapezoid(y_val)
+    print(f'Integralen blir {integral}.')
+
+
+func = lambda x: math.sin(x)
+bound = (0, math.pi)
+steps = 10
+integrate_with_trapeziod(func, bound, steps)
