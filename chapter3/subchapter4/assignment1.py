3ac3cf322a11aee1014db1f988979400a8e197c1
Martin Lidén <martin.liden@live.se>
2022-03-18 21:31:58 +0100

    Added ch3sub4 assignment 1. Bisect method.

---
+++ chapter3/subchapter4/assignment1.py
@@ -0,0 +1,36 @@
+import scipy.optimize as opt
+import numpy as np
+
+def func(x):
+    #return ((x-1)*(x+1))
+    return (x-2)*(x-1)*(x+3)
+    
+def num_bisect(fun, tup, tol = 1e-5):
+    a, b = tup
+    error = np.abs(b-a)
+
+    while error > tol:
+        mid = (a+b)/2
+        
+        if fun(mid) == 0 or np.abs((b-a)/2) < tol:
+            return mid
+    
+        if np.sign(fun(mid)) == np.sign(fun(a)):
+            a = mid
+        else:
+            b = mid
+
+
+#x1 = -2.0
+#x2 = 0.0
+
+#x1 = -4.0
+#x2 = -2.0
+x1 = 1.5
+x2 = 2.7
+
+sci_root = opt.bisect(func, x1, x2, xtol = 1e-5)
+app_root = num_bisect(func, (x1, x2), 1e-3)
+
+print(f'Root given by SciPy: {sci_root:.6f}.')
+print(f'Root given by approximation: {app_root:.6f}.')
